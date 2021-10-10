d0b098805c729d2d27fc99c9683f9455be51dc6d
Mateusz Walter <wuoelte@gmail.com>
2021-10-10 14:58:40 +0200

    zad3 hover menu

--- week5/src/component/NavMenu/AppMenu.js
+++ week5/src/component/NavMenu/AppMenu.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import NavMenu from './NavMenu';
 import "./style.css";
 
 function AppMenu(){
-    
+    const [cli, setClick]=useState(false);
+
+    const handleClick=()=>{
+        if(cli===false){
+            setClick(true);
+        }
+        else{
+            setClick(false);
+        }
+    }
 
     return(
         <div className="app">
-            <div className="menuButton">
-                Menu
-                <NavMenu />
+            <div>
+
+                    <button
+                    className="menuButton"
+                    onClick={handleClick}
+                    >Menu</button>
+                {cli===true ?(
+                        <NavMenu />
+                ) :null }
             </div>
 
 
