88e53f6a5938646ea7c37d0de3f609d21466228e
Mateusz Walter <wuoelte@gmail.com>
2021-10-10 19:27:54 +0200

    zad 3 + modyfikacja DOM

--- week5/src/component/NavMenu/NavMenu.js
+++ week5/src/component/NavMenu/NavMenu.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import "./style.css";
+
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
+
 function NavMenu(){
 
 
     return(
-        <div>
-            <ol className="navMenu">
-                <li className="navOpt"><a>home</a></li>
-                <li className="navOpt"><a>about</a></li>
-                <li className="navOpt"><a>contact</a></li>
-            </ol>
-        </div>
+        <Router>
+            <div >
+                <nav >
+                    <ol className="navMenu">
+                        <li>
+                            <Link to="/"><a>Home</a></Link>
+                        </li>
+                        <li>
+                            <Link to="/about"><a>About</a></Link>
+                        </li>
+                        <li>
+                            <Link to="/contact"><a>Contact</a></Link>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </Router>
     );
 }
 
 export default NavMenu;
+
